73933686489c6f858a6a4c2c17cdaf341e134e06
radoth <activeiy@icloud.com>
2021-10-09 12:38:20 +0800

    Add preprocess and run script.

---
+++ preprocess.sh
@@ -0,0 +1,11 @@
+bpe_path=/data/home/scv0107/run/zyc/125w-zip/subword-nmt
+corpus_path=/data/home/scv0107/run/zyc/125w-zip
+thumt_pth=THUMTA/thumt/scripts
+
+sed -i 's// /g' ${corpus_path}/train.en ${corpus_path}/train.zh
+python ${bpe_path}/learn_joint_bpe_and_vocab.py --input ${corpus_path}/train.en ${corpus_path}/train.zh -s 32000 -o ${corpus_path}/bpecodes --write-vocabulary ${corpus_path}/vocab.en ${corpus_path}/vocab.zh
+python ${bpe_path}/apply_bpe.py -c ${corpus_path}/bpecodes --vocabulary ${corpus_path}/vocab.en --vocabulary-threshold 50 < ${corpus_path}/train.en > ${corpus_path}/train.en.32k
+python ${bpe_path}/apply_bpe.py -c ${corpus_path}/bpecodes --vocabulary ${corpus_path}/vocab.zh --vocabulary-threshold 50 < ${corpus_path}/train.zh > ${corpus_path}/train.zh.32k
+python ${thumt_pth}/shuffle_corpus.py --corpus ${corpus_path}/train.en.32k ${corpus_path}/train.zh.32k
+python ${thumt_pth}/build_vocab.py ${corpus_path}/train.en.32k.shuf ${corpus_path}/vocab.32k.en
+python ${thumt_pth}/build_vocab.py ${corpus_path}/train.zh.32k.shuf ${corpus_path}/vocab.32k.zh
--- run.sh
+++ run.sh
@@ -1,19 +1,31 @@
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-PATH=/usr/local/cuda-10.1/bin:$PATH 
-LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH 
-CODE_PATH=/data/disk5/private/zyc/THUMTD/thumt
+#!/bin/bash
 
-DATA_PATH=/data/disk5/private/zyc/en_zh_data  
-TRAIN_PATH=${DATA_PATH}/train 
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# PATH=/usr/local/cuda-10.1/bin:$PATH 
+# LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH 
+
+module load anaconda/2020.11
+module load cuda/10.2
+module load cudnn/8.1.0.77_CUDA10.2
+source activate thumtc
+
+CODE_PATH=/data/home/scv0107/run/zyc/THUMTA
+
+DATA_PATH=/data/home/scv0107/run/zyc/125w-zip 
+TRAIN_PATH=${DATA_PATH}
 TEST_PATH=${DATA_PATH}/dev_test 
 
-PYTHONPATH=$CODE_PATH:$PYTHONPATH \
-python3 ${CODE_PATH}/bin/trainer.py \
-  --input ${TRAIN_PATH}/train.32k.zh.shuf ${TRAIN_PATH}/train.32k.en.shuf \
+OUTPUT_PATH=/data/home/scv0107/run/zyc/output/test1
+mkdir -p ${OUTPUT_PATH}
+LOGDIR=${OUTPUT_PATH}/`date '+%Y-%m-%d-%H-%M-%S'`
+
+PYTHONPATH=$CODE_PATH \
+python3 -u ${CODE_PATH}/thumt/bin/trainer.py \
+  --input ${TRAIN_PATH}/train.zh.32k.shuf ${TRAIN_PATH}/train.en.32k.shuf \
   --vocabulary ${TRAIN_PATH}/vocab.32k.zh.txt ${TRAIN_PATH}/vocab.32k.en.txt \
   --model transformer \
   --validation ${TEST_PATH}/nist06/nist06.32k.zh \
-  --references ${TEST_PATH}/nist06/nist06.32k.en0 \
-  --output /data/disk5/private/zyc/output \
-  --parameters=batch_size=4096,device_list=[0,1,2,3,4,5,6,7],update_cycle=2,keep_top_checkpoint_max=1 \
-  --hparam_set base
+  --references "${TEST_PATH}/nist06/nist06.en*" \
+  --output ${OUTPUT_PATH} \
+  --parameters=batch_size=16384,device_list=[0,1,2,3],update_cycle=2,keep_top_checkpoint_max=1,eval_steps=500 \
+  --hparam_set bases 2>&1 | tee ${LOGDIR}
